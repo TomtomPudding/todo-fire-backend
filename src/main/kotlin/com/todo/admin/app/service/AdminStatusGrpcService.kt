ac0643d8242e8e8649715fd60670e4aaaeb3dffa
TomtomPudding <tomtomhobby1125@gmail.com>
2021-03-02 04:03:00 +0900

    [add] ログイン状況確認

--- src/main/kotlin/com/todo/admin/app/service/AdminStatusGrpcService.kt
+++ src/main/kotlin/com/todo/admin/app/service/AdminStatusGrpcService.kt
@@ -3,26 +3,27 @@ package com.todo.admin.app.service
 import com.grpc.api.Admin
 import com.grpc.api.AdminStatusServiceCoroutineGrpc
 import com.grpc.api.HttpGrpcStatus
-import com.grpc.api.LoginStatusResponse
 import com.todo.admin.adapter.interceptor.AuthInterceptor
 import com.todo.admin.app.repository.TokenRepository
 import io.grpc.StatusRuntimeException
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.channels.broadcast
+import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import org.lognet.springboot.grpc.GRpcService
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
-import java.util.concurrent.ConcurrentHashMap
 
 @GRpcService(interceptors = [AuthInterceptor::class])
 @ExperimentalCoroutinesApi
 class AdminStatusGrpcService(
     private val tokenRepository: TokenRepository
 ) : AdminStatusServiceCoroutineGrpc.AdminStatusServiceImplBase() {
-
-    private val channels = ConcurrentHashMap.newKeySet<SendChannel<Admin.LoginStatusResponse>>()
-
 
     override suspend fun logout(request: Admin.Empty): Admin.HttpGrpcStatus {
         val userId = AuthInterceptor.USER_IDENTITY.get()
@@ -39,15 +40,28 @@ class AdminStatusGrpcService(
         responseChannel: SendChannel<Admin.LoginStatusResponse>
     ) {
         val userId = AuthInterceptor.USER_IDENTITY.get()
-        channels.forEach {
-            try {
-                tokenRepository.findByIdOrNull(userId) ?: channels.remove(responseChannel)
-                it.send(LoginStatusResponse { status = true })
-            } catch (e: StatusRuntimeException) {
-                channels.remove(responseChannel)
-            }
-            delay(10_00)
+        val authStateChannel = authStateChanel(userId).openSubscription()
+        authStateChannel.consumeEach {
+            responseChannel.send(it)
         }
     }
 
+    private fun authStateChanel(
+        userId: String
+    ) = GlobalScope.broadcast<Admin.LoginStatusResponse> {
+        while (isActive) {
+            try {
+                tokenRepository.findByIdOrNull(userId) ?: cancel()
+                send { status = true }
+            } catch (e: StatusRuntimeException) {
+                send { status = false }
+                cancel(CancellationException(e.message, e))
+            }
+            delay(SEARCH_DELAY)
+        }
+    }
+
+    companion object {
+        private const val SEARCH_DELAY: Long = 1000L
+    }
 }
