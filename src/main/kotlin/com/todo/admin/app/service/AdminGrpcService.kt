635211f4b9258d24957d2845f8bf2def44f874c7
TomtomPudding <tomtomhobby1125@gmail.com>
2021-03-02 04:02:39 +0900

    [add] ゲストログイン機能追加

--- src/main/kotlin/com/todo/admin/app/service/AdminGrpcService.kt
+++ src/main/kotlin/com/todo/admin/app/service/AdminGrpcService.kt
@@ -3,14 +3,17 @@ package com.todo.admin.app.service
 import com.grpc.api.Admin
 import com.grpc.api.AdminServiceCoroutineGrpc
 import com.grpc.api.LoginResponse
+import com.todo.admin.adapter.util.DateUtil.toDate
 import com.todo.admin.app.repository.TokenRepository
 import com.todo.admin.app.repository.UserRepository
 import com.todo.admin.domain.entity.TokenEntity
+import com.todo.admin.domain.entity.UserEntity
 import io.grpc.Status
 import io.grpc.StatusRuntimeException
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.lognet.springboot.grpc.GRpcService
 import org.springframework.data.repository.findByIdOrNull
+import java.time.LocalDate
 import java.util.*
 
 @GRpcService
@@ -34,8 +37,30 @@ class AdminGrpcService(
             ?: tokenRepository.save(TokenEntity(user.userId, UUID.randomUUID().toString()))
 
         return LoginResponse {
-            uid = request.uid
+            uid = user.userId
             email = request.email
+            userName = user.name
+            token = tokenEntity.token
+        }
+    }
+
+    override suspend fun loginGuest(request: Admin.Empty): Admin.LoginResponse {
+        val user = userRepository.save(
+            UserEntity(
+                userId = UUID.randomUUID().toString(),
+                name = "ゲスト"
+            )
+        )
+        val tokenEntity = tokenRepository.save(
+            TokenEntity(
+                userId = user.userId,
+                token = UUID.randomUUID().toString(),
+                expiredAt = LocalDate.now().plusYears(GUEST_EXPIRE).toDate()
+            )
+        )
+        return LoginResponse {
+            uid = user.userId
+            email = ""
             userName = user.name
             token = tokenEntity.token
         }
@@ -44,5 +69,6 @@ class AdminGrpcService(
     companion object {
         private const val INVALID_USER_MESSAGE = "入力が不正です。内容をご確認ください。"
         private const val INVALID_EMPTY_USER_MESSAGE = "ユーザ名を入力してください"
+        private const val GUEST_EXPIRE: Long = 10
     }
 }
